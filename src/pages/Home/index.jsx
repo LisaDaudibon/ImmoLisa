6276fd1a1ea3bcac7fb31939dde73dc39200d135
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-05-16 12:14:07 +0200

    LD starting on page index for Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,12 @@
 import React from "react";
 
-const Home = () => <h1>Home</h1>;
+const Home = () => {
+  return (
+    <>
+      <h1>Home</h1>
+      <p>Grâce à notre savoir-faire, notre expérience et notre écoute, nous accompagnons nos clients dans la création de site internet: étude, UX, conception, design, développement, SEO.
+      Notre agence web est capable de répondre à tous vos besoins et d'élaborer une véritable stratégie digitale.</p>
+    </>);
+};
+
 export default Home;
